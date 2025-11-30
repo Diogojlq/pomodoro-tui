ae53cacaa686e13cebcaa5c1a2a862c992011266
Diogo <fmlsp2015@gmail.com>
2025-11-30 11:24:02 -0300

    fix: import all needed packages

--- pomodoro.py
+++ pomodoro.py
@@ -1,25 +1,63 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Static
+from textual.widgets import Static, Button, Header, Footer
+from textual.containers import Container
 from textual.screen import Screen
 
-class Timer(Static):
+class TimerDisplay(Static):
+    """Screen for the timer display."""
+    def __init__(self, total_seconds):
+        super().__init__()
+        self.total_seconds = total_seconds
+        self.timer_interval = None
 
     def on_mount(self):
-        self.seconds = 10
-        self.update(f"⏳ {self.seconds}")
-        self.set_interval(1, self.update_timer)
+        self.update_timer()
+        self.timer_interval = self.set_interval(1, self.tick)
+
+    def tick(self):
+        self.total_seconds -= 1
+        if self.total_seconds <= 0:
+            self.update("Time is over!")
+            if self.timer_interval:
+                self.timer_interval.stop()
+        else:
+            self.update_timer()
 
     def update_timer(self):
-        self.seconds -= 1
-        if self.seconds <= 0:
-            self.update("⏱️ Tempo esgotado!")
-            self.set_interval(0, lambda: None)  # Para o timer
-        else:
-            self.update(f"⏳ {self.seconds}")
+        minutes, seconds = divmod(self.total_seconds, 60)
+        self.update(f"{minutes:02d}:{seconds:02d}")
+
+class TimeSelector(Static):
+    """Buttons for setting the time."""
+    def compose(self) -> ComposeResult:
+        yield Static("How much time you want to focus?", classes="title")
+        
+        with Container(classes="buttons-container"):
+            yield Button("25 Min", id="btn-25", variant="success")
+            yield Button("30 Min", id="btn-30", variant="warning")
+            yield Button("45 Min", id="btn-45", variant="error")
+
+class TimerScreen(Screen):
+    """Screen for the."""
+    
+    def __init__(self, seconds):
+        super().__init__()
+        self.seconds = seconds
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        with Container(id="box"):
+            yield TimerDisplay(self.seconds)
+            yield Button("Cancelar / Voltar", id="btn-back", variant="default")
+        yield Footer()
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "btn-back":
+            self.dismiss()
 
 class TimerApp(App):
     def compose(self) -> ComposeResult:
-        yield Timer()
+        yield TimerDisplay()
 
 if __name__ == "__main__":
     TimerApp().run()
